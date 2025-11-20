a9daa2105844257828a17d24dd4fe2b3ce04faaf
sandrow488 <sandropegorarow48@gmail.com>
2025-11-20 19:13:51 +0100

    Add files via upload

---
+++ MiniCRUD/src/SociogramaDAW/includes/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sociograma DAW</title>
+    <link rel="icon" type="image/x-icon" href="assets/img/image-removebg-preview.png">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <script src="../assets/js/validate.js" defer></script>
+    
+</head>
+<body>
+    
